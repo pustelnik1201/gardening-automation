978da5c016be56d5ec95d709620dad92511ebbae
pustelnik1201 <73897268+pustelnik1201@users.noreply.github.com>
2024-09-26 17:49:36 +0200

    first upload

---
+++ gardening.py
@@ -0,0 +1,113 @@
+import network
+from machine import Pin, ADC
+from time import ticks_ms
+PI = 3.1416
+
+# used if params not given in constructors
+PUMP_SPEED 	= 34 # [ml/s]
+TUBE_DIAM	= 5  # [mm]
+WATER_MLS	= 50 # [ml]
+
+class Sector:	
+    # constructor
+    def __init__(self, pump_pin, soil_moist_pin, soil_moist_to_water, tube_length, water_mls=WATER_MLS, tube_diam=TUBE_DIAM, pump_speed=PUMP_SPEED):
+        # init sector's pump
+        self.pump = Pin(pump_pin, Pin.OUT)
+        self.pump.value(0)
+        
+        # init sector's sensor
+        self.soil_moist = ADC(Pin(soil_moist_pin))
+        self.soil_moist.atten(ADC.ATTN_11DB)
+        self.soil_moist_read = self.soil_moist.read()
+        
+        # moisture indicating a need for watering
+        self.soil_moist_low = soil_moist_to_low 
+        
+        self.water_mls = water_mls
+        # V = PI * r^2 * length [mm^3] -> *0.001 -> [ml]
+        self.tube_volume = (tube_diam/2) * (tube_diam/2) * tube_length * 0.001
+        self.pump_speed = pump_speed
+        # used for watering automatically
+        self.needs_watering = False
+    
+    def Sectors(self, pump_pins, moist_pins, water_mls, tube_length, tube_diam=None, pump_speed=None):
+        # assign global lib tube diameter if not given
+        if tube_diam is None:
+            tube_diam = [TUBE_DIAM] * len(pump_pins)  
+        # assign global lib pump speed if not given
+        if pump_speed is None:
+            pump_speed = [PUMP_SPEED] * len(pump_pins)
+        # construct a list of Sector objects
+        sectors = [
+            Sector(pump_pins[i], moist_pins[i], water_mls[i], tube_length[i], tube_diam=tube_diam[i], pump_speed=pump_speed[i])
+            for i in range(len(pump_pins))
+            ]
+        return sectors
+    
+    # reading moisture and checking whether watering needed
+    def read_moist(sectors, automatic=False):
+        # checking if sectors is a single Sector:
+        if isinstance(sectors, Sector):
+            sectors.soil_moist_read = self.soil_noist.read()
+            if sectors.soil_moist_read > sectors.soil_moist_low:
+                sectors.needs_watering = True
+        # checking if sectors is a list of Sectors
+        elif isinstance(sectors, list):
+            for x in sectors:
+                sectors[x].soil_moist_read = self.soil_moist.read()
+                if sectors[x].soil_moist_read > sectors[x].soil_moist_low:
+                    sectors[x].needs_watering = True
+    
+    #  watering every time when called
+    def water(self, water_mls=None):
+        # taking object mls as a default
+        if water_mls is None:
+            water_mls = self.water_mls
+        
+        # time for controlling watering time
+        water_current_time = ticks_ms()
+        water_prev_time = water_current_time
+        
+        print('volume: ', self.tube_volume, ' [mm^3]')
+        print('water amount: ', water_mls, ' [mL]')
+        # calculating how long to pump water
+        water_time = float((self.tube_volume + water_mls)) / float(self.pump_speed)
+        
+        print('watering for: ', water_time, ' [s]')
+        # powering pump for calculated period
+        while water_current_time - water_prev_time < water_time * 1000:
+            self.pump.value(1)
+            water_current_time = ticks_ms()
+            #print('...watering: ', water_current_time - water_prev_time, ' [s]')
+        print('watering done!')
+        self.pump.value(0)
+    
+    # watering when sector needs it
+    def water_automatically(sectors):
+        # checking if sectors is a single Sector
+        if isinstance(sectors, Sector):
+            #watering sector if water needed
+            if sectors.needs_watering:
+                sectors.water()
+                print('Sector watered!')
+        # checking if sectors is a list of Sectors
+        elif isinstance(sectors, list):
+            # watering all sectors needing water
+            for x in sectors:
+                if sectors[x].needs_watering:   
+                    sectors[x].water()
+                    print('Sector ', x, ' watered!')
+
+# initialising wifi connection
+def connect_to_wifi(WIFI_SSID, WIFI_PASS):
+    wifi = network.WLAN(network.STA_IF)
+    if not wifi.isconnected():
+        print("Connecting to WiFi...")
+        wifi.active(True)
+        wifi.connect(WIFI_SSID, WIFI_PASS)
+        while not wifi.isconnected():
+            pass
+    print('IP:', wifi.ifconfig()[0])
+    
+
+
---
+++ main.py
@@ -0,0 +1,103 @@
+import BlynkLib
+import network
+from machine import Pin, idle, ADC
+from time import sleep, ticks_ms
+from gardening import Sector, Sectors, connect_to_wifi
+
+# credentials to connect to Wifi and Blynk IoT
+BLYNK_AUTH = 'jcqlyDh5d9SWLrI0WT2NnmgIYebNHznO'
+WIFI_SSID = 'SecretNetwork'
+WIFI_PASS = 'onom@topeiA123'
+
+water_mls1 = 300
+
+pump_speed = [40, 34, 34, 34]
+# PIN rundown
+pump_pins 		= [16, 2, 19, 5]
+moist_pins 		= [36, 39, 34, 35]
+sensor_pins 	= [32, 33, 25]
+
+# Sector params
+water_mls 		= [water_mls1, 100, 100, 100]
+tube_length		= [50, 100, 100, 100]
+tube_diam		= [5.5, 5.5, 5.5, 5.5]
+moist_lowest 	= [1800, 1800, 1800, 1800] 
+
+led = Pin(27, Pin.OUT)
+# button = Pin(#, Pin.IN)
+
+# Sectors definition
+# (pump pin, soil moist pin, tube length)
+sectors = Sectors(pump_pins, moist_pins, moist_lowest, tube_length, water_mls=water_mls, tube_diam=tube_diam, pump_speed=pump_speed)
+
+#---INIT START---
+led.value(1)
+#blynk_disconnected = True
+
+# Connecting to Wifi
+connect_to_wifi(WIFI_SSID, WIFI_PASS)
+
+# Connecting to Blynk IoT
+blynk = BlynkLib.Blynk(BLYNK_AUTH)
+
+#def connect_to_Blynk(b, b_disconnected):
+#    while b_disconnected == True:
+#        print('trying to reconnect')
+#        b.connect()
+#        sleep(5)
+    
+# Blynk event handlings
+@blynk.on("connected")
+def blynk_connected(ping):
+    print('Blynk ready. Ping:', ping, 'ms')
+    #---INIT END---
+    led.value(0)
+    blink_disconnected = False
+
+@blynk.on("disconnected")
+def blynk_disconnected():
+    print('Blynk disconnected')
+    led.value(1)
+    blynk_disconnected = True
+
+@blynk.on("V1")
+def v0_write_handler(value):
+    print('Current slider value: {}'.format(value[0]))
+    sectors[0].water_mls = int(value[0])
+    print('water_mls: ', sectors[0].water_mls)
+
+@blynk.on("V27")
+def v0_write_handler(value):
+    print('Current slider value: {}'.format(value[0]))
+    slider_state = int(value[0])
+   
+    if slider_state is 0:
+        led.value(0)
+    else:
+        led.value(1)
+    sectors[0].water()
+    
+def write_analog_value(value):
+    # Ensure value is within the range
+    if 0 <= value <= 2800:
+        blynk.virtual_write(36, value)  # Writing to V36
+        print(f"Wrote {value} to V36")
+    else:
+        print("Value out of range. Must be between 0 and 2500.")
+    
+led.value(0)
+while True:
+    # mapping current time
+    #current_time = ticks_ms() * 1000 # [s]
+    #connect_to_Blynk(blynk, blynk_disconnected)
+    
+    blynk.run()
+    Sector.read_moist(sectors)
+    write_analog_value(sectors[0].soil_moist_read)
+    sleep(5)
+
+# You can also run blynk in a separate thread (ESP32 only)
+#import _thread
+#_thread.stack_size(5*1024)
+#_thread.start_new_thread(runLoop, ())
+
